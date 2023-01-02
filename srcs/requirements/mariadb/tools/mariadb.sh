8006d639ce0ebbc12098294c842e3922a302abb8
ouzhamza <ouzhamza@ouzhamza>
2023-01-02 17:47:55 +0100

    testing to evaluate

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -2,16 +2,14 @@
 
 sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mysql/mariadb.conf.d/50-server.cnf
 
-/etc/init.d/mysql start
+service mysqld start
 
 if [ ! -d "/var/lib/mysql/$SQL_DATABASE" ]
 then
 	# create wordpress database
 	echo "CREATE DATABASE $SQL_DATABASE; GRANT ALL ON $SQL_DATABASE.* TO '$SQL_USER'@'%' IDENTIFIED BY '$SQL_USER_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root
-#	Set the password for the root user
-#	echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$SQL_ROOT_PASSWORD');" | mysql -u root
+	Set the password for the root user
+	echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$SQL_ROOT_PASSWORD');" | mysql -u root
 fi
-
-/etc/init.d/mysql stop
 
 exec "$@"
